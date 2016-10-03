fa0d36b11a5f1b4da05593a91a73d36aa773fa8b
NUMBER0-PC\Smati <stungkit@gmail.com>
2016-10-03 00:09:13 -0700

    Trying to fix PostgreSQL since the datetime is not consistent across sqlite3.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import UserIP
+import datetime
 import json
 
 
@@ -10,8 +11,13 @@ def index(request):
 		queried_data = UserIP.objects.filter(ip_address=request.GET["current_ip_address"]).values("datetime", "frequency").order_by("datetime")
 		json_response = {"data": []}
 		for entry in queried_data:
-			# json.dumps() cannot serialize datetime object so we need .isoformat() to conver it to String
-			formatted_datetime = entry["datetime"].strftime("%b %d, %Y at %I:%M:%S %p")
+			# json.dumps() cannot serialize datetime object so we need .isoformat() to conver it to String			
+			if isinstance(entry["datetime"] , datetime.datetime) is True:
+				# Works in sqlite3	
+				formatted_datetime = entry["datetime"].strftime("%b %d, %Y at %I:%M:%S %p")
+			else:
+				# Works in PostgreSQL
+				formatted_datetime = datetime.datetime.strptime(entry["datetime"], "%Y-%m-%d %H:%M:%S").strftime("%b %d, %Y at %I:%M:%S %p")
 			json_response["data"].append([formatted_datetime, entry["frequency"]])
 		return HttpResponse(json.dumps(json_response), content_type='application/json')
 
