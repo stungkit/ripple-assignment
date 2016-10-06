d41fbac7a7bb8af6ea4a092b86fe5c2e09a37ced
NUMBER0-PC\Smati <stungkit@gmail.com>
2016-10-05 23:49:22 -0700

    Fixing deployment issue.

--- ripple/settings.py
+++ ripple/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
